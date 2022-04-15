c47f7cdbc83a9ee4cf66b2b55a36673cc9436d17
mlrad <103540995+mlrad@users.noreply.github.com>
2022-04-15 02:35:19 +0200

    Initial commit

---
+++ src/Form.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Form(props) {
+  const { movie } = props;
+
+  return (
+    <form>
+      <h3>{movie.name}</h3>
+      <button> - </button>
+      <button> 0 </button>
+      <button> + </button>
+    </form>
+  );
+}
