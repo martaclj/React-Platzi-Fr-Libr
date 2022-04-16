9e3c71367b2c0977b59690a252a1aae6821dfcca
martaclj <104948192+martaclj@users.noreply.github.com>
2022-04-16 16:11:00 +0200

    movie tickets

--- src/Form.js
+++ src/Form.js
@@ -1,14 +1,20 @@
 import React from "react";
 
 export default function Form(props) {
+  const [quantity, setQuantity] = React.useState(0);
   const { movie } = props;
 
   return (
     <form>
       <h3>{movie.name}</h3>
-      <button type="button"> - </button>
-      <button> 0 </button>
-      <button type="button"> + </button>
+      <button type="button" onClick={() => setQuantity(quantity - 1)}>
+        {" "}
+        -{" "}
+      </button>
+      {quantity}
+      <button type="button" onClick={() => setQuantity(quantity + 1)}>
+        +
+      </button>
     </form>
   );
 }
